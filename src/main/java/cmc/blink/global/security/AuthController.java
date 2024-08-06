f526bd5ff54261a5aa01bd1c4943b85866e134e3
suminnnnn <tlsssm1212@ajou.ac.kr>
2024-08-07 04:13:10 +0900

    Docs: 스웨거 설명 작성

--- src/main/java/cmc/blink/global/security/AuthController.java
+++ src/main/java/cmc/blink/global/security/AuthController.java
@@ -2,6 +2,7 @@ package cmc.blink.global.security;
 
 import cmc.blink.global.common.ApiResponseDto;
 import cmc.blink.global.security.dto.AuthResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ public class AuthController {
     private final AuthService authService;
 
     @GetMapping("/oauth2/login/oauth2/code/google")
+    @Operation(summary = "구글 로그인 API", description = "구글 로그인 페이지에서 리디렉션 되어 토큰 응답 하는 API입니다.")
     public ApiResponseDto<AuthResponse.LoginResponseDto> googleLogin(@RequestParam(name ="code") String code){
         return ApiResponseDto.of(authService.googleLogin(code));
     }
