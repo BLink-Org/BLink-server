189d8203930610acf182f384db779bb047984280
suminnnnn <tlsssm1212@ajou.ac.kr>
2024-08-14 12:03:13 +0900

    Refactor: 구글 로그인 Feign Client Config 설정

--- src/main/java/cmc/blink/global/security/client/GoogleTokenVerifierClient.java
+++ src/main/java/cmc/blink/global/security/client/GoogleTokenVerifierClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "googleTokenVerifierClient", url = "https://oauth2.googleapis.com")
+@FeignClient(name = "googleTokenVerifierClient", url = "https://oauth2.googleapis.com", configuration = GoogleFeignClientConfig.class)
 public interface GoogleTokenVerifierClient {
 
     @GetMapping("/tokeninfo")
