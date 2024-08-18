ad2dbe61836c2671c471a985fa84723999ff4ec7
suminnnnn <tlsssm1212@ajou.ac.kr>
2024-08-18 21:10:05 +0900

    Hotfix: 폴더, 링크명 입력 가능 최대 바이트 수정

--- src/main/java/cmc/blink/domain/link/presentation/dto/LinkRequest.java
+++ src/main/java/cmc/blink/domain/link/presentation/dto/LinkRequest.java
@@ -32,7 +32,7 @@ public class LinkRequest {
 
     @Getter
     public static class LinkTitleUpdateDto {
-        @ByteSize(max = 400, message = "링크 제목은 최대 300바이트 까지 입력할 수 있습니다.")
+        @ByteSize(max = 500, message = "링크 제목은 최대 300바이트 까지 입력할 수 있습니다.")
         @NotBlank(message = "링크 제목은 공백으로 설정할 수 없습니다.")
         String title;
     }
