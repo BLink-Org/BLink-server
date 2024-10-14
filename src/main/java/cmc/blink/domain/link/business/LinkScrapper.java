8fdb4ed1f995bf53a9317db4eb251e0f0cbf0556
suminnnnn <tlsssm1212@ajou.ac.kr>
2024-10-14 10:42:47 +0900

    Refactor: 링크 스크래핑 클래스 분리

---
+++ src/main/java/cmc/blink/domain/link/business/LinkScrapper.java
@@ -0,0 +1,10 @@
+package cmc.blink.domain.link.business;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LinkScrapper {
+
+}
